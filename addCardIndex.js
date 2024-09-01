1ac4b03c01efaaf944e559e093dea8e02506138a
Hakan Ozdemir <hak.ozdmr@hotmail.com>
2024-09-01 21:48:59 +0300

    İlk commit

---
+++ addCardIndex.js
@@ -0,0 +1,176 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js';
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js';
+
+// Firebase yapılandırmanızı burada yapın
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ9b_kW8x_g0MPseuYZvNKFFzY62V8yRM",
+  authDomain: "koktensifa-8b9d6.firebaseapp.com",
+  projectId: "koktensifa-8b9d6",
+  storageBucket: "koktensifa-8b9d6.appspot.com",
+  messagingSenderId: "430082742734",
+  appId: "1:430082742734:web:e9f7e56af807b8ad56b39f"
+};
+
+// Firebase'i başlatın
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+document.addEventListener('DOMContentLoaded', async function() {
+  var cardsContainer = document.querySelector('#section-certificate .slider');
+  if (!cardsContainer) {
+    console.error("cardsContainer element not found!");
+    return;
+  }
+
+  try {
+    // Firestore'dan veriyi çekin
+    const querySnapshot = await getDocs(collection(db, "cards"));
+    const cards = querySnapshot.docs.map(doc => doc.data());
+    console.log(cards); // Verileri konsola yazdırın
+
+    function createSlides(cards) {
+      var slides = [];
+
+      if (cards.length === 0) {
+        // Kart bulunamadığında mesaj göster
+        slides.push(`<div class="slide row">
+          <h1 class="heading-secondary">Sertifika bulunamadı</h1></div>`);
+      } else {
+        for (var i = 0; i < cards.length; i += 3) {
+          var slideContent = `<div class="slide row">`;
+          var hasContent = false;
+          var cardCount = 0;
+
+          for (var j = i; j < i + 3 && j < cards.length; j++) {
+            var card = cards[j];
+            var dateDifference = card.dateDifference || { years: 0, months: 0, days: 0 }; // Varsayılan değerler
+
+            slideContent += `
+              <div class="col-1-of-3">
+                <div class="card">
+                  <div class="card__side card__side--front">
+                    <div class="card__picture card__picture--${j}" style="background-image: url('${card.image}');">
+                    &nbsp;</div>
+                    <h4 class="card__heading">
+                      <span class="card__heading--span card__heading--span--${j}">${card.title}</span>
+                    </h4>
+                    <div class="card__details">
+                      <ul>
+                        <li>${card.description}</li>
+                        <li>Tarih: ${card.date}</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="card__side card__side--back card__side--back-${j}">
+                    <div class="card__cta">
+                      <div class="card__price-box">
+                        <p class="card__price-only">${card.title}</p>
+                        <p class="card__price-value">Geçen süre: ${dateDifference.years} yıl, ${dateDifference.months} ay, ${dateDifference.days} gün</p>
+                      </div>
+                      <a href="#section-book" class="btn btn--white card__cta__btn">Sen de Katıl!</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            `;
+            hasContent = true;
+            cardCount++;
+          }
+
+          // Boş alan ekleme mantığı
+          if (cardCount === 1) {
+            slideContent += `
+              <div class="col-1-of-3">&nbsp;</div>
+              <div class="col-1-of-3">&nbsp;</div>
+            `;
+          } else if (cardCount === 2) {
+            slideContent += `
+              <div class="col-1-of-3">&nbsp;</div>
+            `;
+          }
+
+          if (hasContent) {
+            slideContent += `</div>`;
+            slides.push(slideContent);
+          }
+        }
+      }
+
+      return slides;
+    }
+
+    function renderSlides(slides) {
+      cardsContainer.innerHTML = '';
+      slides.forEach(function(slide) {
+        cardsContainer.insertAdjacentHTML('beforeend', slide);
+      });
+    }
+
+    function updateSlider() {
+      const slider = document.querySelector('.slider');
+      let currentIndex = 0;
+
+      function updateSlidePosition() {
+        slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+      }
+
+      function updateButtons() {
+        const prevButton = document.querySelector('.prevSlider');
+        const nextButton = document.querySelector('.nextSlider');
+
+        if (slides.length <= 1) {
+          prevButton.style.display = 'none';
+          nextButton.style.display = 'none';
+        } else {
+          prevButton.style.display = currentIndex === 0 ? 'none' : 'block';
+          nextButton.style.display = currentIndex === slides.length - 1 ? 'none' : 'block';
+        }
+      }
+
+      document.querySelector('.nextSlider').addEventListener('click', function() {
+        if (currentIndex < slides.length - 1) {
+          currentIndex++;
+          updateSlidePosition();
+          updateButtons();
+        }
+      });
+
+      document.querySelector('.prevSlider').addEventListener('click', function() {
+        if (currentIndex > 0) {
+          currentIndex--;
+          updateSlidePosition();
+          updateButtons();
+        }
+      });
+
+      // Initialize button states
+      updateButtons();
+    }
+
+    function filterCards() {
+      var cardName = document.querySelector('#card-name').value.toLowerCase();
+      var cardDescription = document.querySelector('#card-description').value.toLowerCase();
+      
+      var filteredCards = cards.filter(function(card) {
+        var titleMatch = cardName === '' || card.title.toLowerCase().includes(cardName);
+        var descriptionMatch = cardDescription === '' || card.description.toLowerCase().includes(cardDescription);
+        return titleMatch && descriptionMatch;
+      });
+
+      var slides = createSlides(filteredCards);
+      renderSlides(slides);
+      updateSlider();
+    }
+
+    // Event listeners for input changes
+    document.querySelector('#card-name').addEventListener('input', filterCards);
+    document.querySelector('#card-description').addEventListener('input', filterCards);
+
+    // Initial render
+    var slides = createSlides(cards);
+    renderSlides(slides);
+    updateSlider();
+  } catch (error) {
+    console.error("Firestore'dan veri çekilirken hata oluştu: ", error);
+  }
+});
---
+++ app.js
@@ -0,0 +1,191 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js';
+import { getFirestore, collection, addDoc, getDoc, deleteDoc, doc, getDocs } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js';
+
+// Firebase yapılandırması
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ9b_kW8x_g0MPseuYZvNKFFzY62V8yRM",
+  authDomain: "koktensifa-8b9d6.firebaseapp.com",
+  projectId: "koktensifa-8b9d6",
+  storageBucket: "koktensifa-8b9d6.appspot.com",
+  messagingSenderId: "430082742734",
+  appId: "1:430082742734:web:e9f7e56af807b8ad56b39f"
+};
+
+// Firebase'i başlat
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Maksimum deneme hakkı
+const MAX_ATTEMPTS = 3;
+let attemptCount = 0;
+
+async function checkPassword() {
+  while (attemptCount < MAX_ATTEMPTS) {
+    const userPassword = prompt("Şifre Giriniz...");
+
+    if (userPassword === null) {
+      // Kullanıcı "İptal" butonuna bastıysa
+      attemptCount++;
+      if (attemptCount >= MAX_ATTEMPTS) {
+        console.log("Deneme hakkınız bitti.");
+        alert("Deneme hakkınız bitti. Sayfa yüklenmeyecek.");
+        document.body.innerHTML = "<h1>Deneme hakkınız bitti. Sayfa yüklenmedi.</h1>";
+        return;
+      } else {
+        alert(`İptal edildi. Kalan deneme hakkınız: ${MAX_ATTEMPTS - attemptCount}`);
+        continue;
+      }
+    }
+
+    try {
+      // Belirtilen konumdan veriyi al
+      const docRef = doc(db, 'password', '2');
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const storedPassword = docSnap.data().password; // Şifreyi al
+
+        if (userPassword === storedPassword) {
+          console.log("Şifre doğru!");
+          return; // Şifre doğruysa fonksiyonu bitir
+        } else {
+          console.log("Şifre yanlış!");
+          attemptCount++;
+          alert(`Şifre yanlış! Kalan deneme hakkınız: ${MAX_ATTEMPTS - attemptCount}`);
+        }
+      } else {
+        console.log('Şifre verisi bulunamadı');
+        alert('Şifre verisi bulunamadı.');
+        document.body.innerHTML = "<h1>Şifre verisi bulunamadı. Sayfa yüklenmedi.</h1>";
+        return;
+      }
+    } catch (error) {
+      console.error('Şifre doğrulama sırasında hata oluştu:', error);
+      alert('Şifre doğrulama sırasında bir hata oluştu.');
+      document.body.innerHTML = "<h1>Bir hata oluştu. Sayfa yüklenmedi.</h1>";
+      return;
+    }
+  }
+
+  // Maksimum deneme hakkı aşıldığında
+  console.log("Deneme hakkınız bitti.");
+  alert("Deneme hakkınız bitti. Sayfa yüklenmeyecek.");
+  document.body.innerHTML = "<h1>Deneme hakkınız bitti. Sayfa yüklenmedi.</h1>";
+}
+
+// Kart ekleme işlevi
+document.getElementById("create-card-btn").addEventListener("click", async function (event) {
+  event.preventDefault();
+
+  var cardName = document.getElementById("card-name").value;
+  var cardDescription = document.getElementById("card-description").value;
+  var certDate = document.getElementById("cert-date").value;
+  var cardImageInput = document.getElementById("cardImage");
+
+  var file = cardImageInput.files[0];
+  var reader = new FileReader();
+
+  function calculateDateDifference(startDate) {
+    const today = new Date();
+    const start = new Date(startDate);
+
+    let years = today.getFullYear() - start.getFullYear();
+    let months = today.getMonth() - start.getMonth();
+    let days = today.getDate() - start.getDate();
+
+    if (days < 0) {
+      months--;
+      const lastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+      days += lastMonth.getDate();
+    }
+
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    return {
+      years: years,
+      months: months,
+      days: days,
+    };
+  }
+
+  reader.onloadend = async function () {
+    var cardImage = reader.result;
+    var dateDifference = calculateDateDifference(certDate);
+
+    try {
+      await addDoc(collection(db, 'cards'), {
+        title: cardName,
+        description: cardDescription,
+        date: certDate,
+        image: cardImage,
+        dateDifference: dateDifference,
+        timestamp: new Date()
+      });
+      console.log('Kart başarıyla Firebase\'e eklendi');
+      loadCards(); // Kartları yeniden yükle
+    } catch (error) {
+      console.error('Kart eklenirken hata oluştu:', error);
+    }
+  };
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    console.error("No file selected.");
+  }
+});
+
+// Kartları Firebase'den yükleyip tabloya ekleme işlevi
+async function loadCards() {
+  const cardsTableBody = document.querySelector('#cardsTable tbody');
+  if (!cardsTableBody) {
+    console.error("cardsTableBody element not found!");
+    return;
+  }
+
+  try {
+    const querySnapshot = await getDocs(collection(db, 'cards'));
+    cardsTableBody.innerHTML = ''; // Önceki verileri temizle
+
+    querySnapshot.forEach((doc) => {
+      const card = doc.data();
+      const cardId = doc.id;
+      const row = document.createElement('tr');
+
+      row.innerHTML = `
+        <td>${card.title}</td>
+        <td>${card.description}</td>
+        <td>${card.date}</td>
+        <td><img src="${card.image}" alt="Card Image" style="width: 100px; height: auto;"></td>
+        <td><button class="delete-btn" data-id="${cardId}">Sil</button></td>
+      `;
+
+      cardsTableBody.appendChild(row);
+    });
+
+    // Silme butonlarına tıklama olayı ekle
+    document.querySelectorAll('.delete-btn').forEach(button => {
+      button.addEventListener('click', async (event) => {
+        const cardId = event.target.getAttribute('data-id');
+        try {
+          await deleteDoc(doc(db, 'cards', cardId));
+          console.log('Kart başarıyla silindi');
+          loadCards(); // Güncel kartları tekrar yükle
+        } catch (error) {
+          console.error('Kart silinirken hata oluştu:', error);
+        }
+      });
+    });
+  } catch (error) {
+    console.error('Veriler çekilirken hata oluştu:', error);
+  }
+}
+
+// Sayfa yüklendiğinde kartları yükle
+document.addEventListener('DOMContentLoaded', loadCards);
+
+// Şifre kontrolünü başlat
+checkPassword();
